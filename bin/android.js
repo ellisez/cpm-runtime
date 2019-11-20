e0c29c6f15bd42172eed074864cfe40fdb7b2f0b
ellise <ellise@qq.com>
2019-11-21 02:03:59 +0800

    init

---
+++ bin/android.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+process.env.CPM_PLATFORM='android';
+
+const root=path.resolve(cwd, 'native', cpm_platform);
+if (!fs.existsSync(root)) {
+  console.error(`${cpm_platform} are not installed!\ntry to add platform ${cpm_platform} to "package.json".\ntry to "cpm build".`);
+  return;
+}
+
+process.env.VUE_CLI_SERVICE_CONFIG_PATH=path.resolve(root, 'vue.config.js');
+
+require('../lib/platform');
